35f8a6ebd135ef2364eb3bfa36e266a31dd00a8a
Koemy <francine.pns@gmail.com>
2022-09-04 21:01:32 -0300

    Create section

---
+++ src/components/Section.tsx
@@ -0,0 +1,33 @@
+import Product from './Product';
+
+interface CardapioContent {
+    title: string,
+    section: Content[],
+}
+
+interface Content {
+    name: string,
+    price: string,
+    description: string,
+}
+
+function Section ({title, section}: CardapioContent) {
+    return (
+        <div>
+            <h1 className="grid content-center justify-center">{title}</h1>
+            <div>
+                {
+                    section.map(section => {
+                        return (
+                            <Product name={section.name}
+                                     price={section.price}
+                                     description={section.description}/>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Section
