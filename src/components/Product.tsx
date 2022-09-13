a1537f76f4a1a6b0bdf0b0f1d6a243c89d81bfc2
Koemy <francine.pns@gmail.com>
2022-09-13 11:59:04 -0300

    Modify product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -9,20 +9,20 @@ interface Content {
 
 function Product({name, price, description, img, alt, tag}: Content) {
 	return (
-		<div className="flex text-center justify-center items-center">
+		<div className="flex justify-center items-center">
 			<div className="w-20 py-3 flex mx-5">
 				<img src={img} alt={alt}/>
 			</div>
-			<div className="w-64">
-				<div className="flex py-2 justify-between">
-					<p className="text-orange-400 font-semibold break-after-auto">{name}</p>
+			<div className="max-w-xs min-w-0">
+				<div className="w-64 py-2">
+					<p className="break-words text-orange-400 font-semibold">{name}</p>
 				</div>
 				<div className="flex items-center justify-center">
 					<p className="mb-3 w-16 text-green-500 border border-green-500 rounded">{tag}</p>
 				</div>
 				<p className="px-2 flex justify-center w-64 text-white-300"> {description}</p>
 				<div className="flex">
-					<p className="text-white-300 py-1">R$ {price}</p>
+					<p className="text-white-300 py-2">R$ {price}</p>
 				</div>
 			</div>
 		</div>
