cdbb5a3669c97202d3cfacfc332e071c66862153
Koemy <francine.pns@gmail.com>
2022-09-20 13:33:51 -0300

    Add new colors and media size

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,9 +23,28 @@ module.exports = {
 			},
 			'green': {
 				'500': 'rgb(34 197 94)',
+			},
+			'grey': {
+				'300': 'rgb(209 213 219)'
 			}
 		},
 		extend: {},
+	},
+	screens: {
+		'sm': '640px',
+		// => @media (min-width: 640px) { ... }
+
+		'md': '768px',
+		// => @media (min-width: 768px) { ... }
+
+		'lg': '1024px',
+		// => @media (min-width: 1024px) { ... }
+
+		'xl': '1280px',
+		// => @media (min-width: 1280px) { ... }
+
+		'2xl': '1536px',
+		// => @media (min-width: 1536px) { ... }
 	},
 	plugins: [],
 };
