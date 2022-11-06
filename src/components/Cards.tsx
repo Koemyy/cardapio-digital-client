d3758941a05f2c0c70f82c91d8e9a5dfe0ae672e
Koemy <francine.pns@gmail.com>
2022-11-06 15:45:48 -0300

    Modify cards and add style

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -17,16 +17,18 @@ interface CardsContent {
 function Cards({title, sectionCards}: CardsSection) {
 	return (
 		<div>
-			<h1 className="text-orange-700 flex text-xl py-5 font-bold justify-center items-center">{title}</h1>
-			<div>
+			<h1 className="text-orange-500 flex text-xl py-5 font-bold justify-center items-center">{title}</h1>
+			<div className="flex">
 				{
 					sectionCards.map(sectionCards => {
 						return (
-							<CardsSection key={sectionCards.name} name={sectionCards.name}
-								price={sectionCards.price}
-								oldPrice={sectionCards.oldPrice}
-								description={sectionCards.description} img={sectionCards.img}
-								tag={sectionCards.tag}/>
+							<section key={sectionCards.name} className="inline-block px-5">
+								<CardsSection key={sectionCards.name} name={sectionCards.name}
+									price={sectionCards.price}
+									oldPrice={sectionCards.oldPrice}
+									description={sectionCards.description} img={sectionCards.img}
+								/>
+							</section>
 						);
 					})
 				}
--- src/components/CardsSection.tsx
+++ src/components/CardsSection.tsx
@@ -1,40 +1,21 @@
-import {useEffect} from 'react';
-import {useState} from 'react';
-
 interface CardsContent {
     name: string,
     price: string,
     oldPrice: string,
     description: string,
     img: string,
-    tag: string | null,
+    tag?: string,
 }
 
-function CardsSection({name, price, oldPrice, description, img, tag}: CardsContent) {
-	/*
-        useEffect(() => {
-            fetch('http://localhost:3000/Cardapio.json')
-                .then((response) => response.json())
-                .then(console.log);
-        }, []);
-    */
+function CardsSection({name, price, oldPrice, img}: CardsContent) {
 	return (
-		<div className="flex box-border mx-3 justify-center items-center">
-			<div className="sm:w-32 md:w-48 lg:w-96 py-3 rounded overflow-hidden shadow-lg">
-				<div className="sm:w-32 md:w-48 lg:w-96">
-					<img className="w-full" src={img}/>
-				</div>
-				<div className="px-6 py-4">
-					<div className="break-words font-bold text-orange-500 mb-2">{name}</div>
-					<p className="text-white-300">{description}</p>
-				</div>
-				<div className="px-6 pb-3">
-					<span
-						className="flex items-center justify-center mb-3 w-16 text-green-500 border border-green-500 rounded">{tag}</span>
-					<span
-						className="px-3 text-white-300">R$ {price}</span>
-					<span
-						className="px-3 text-grey-300 line-through">R$ {oldPrice}</span>
+		<div className="lg:w-full hover:border-4 border-white-300 ease-in-out duration-100 relative my-5 align-middle">
+			<img className="cursor-pointer w-[600px] h-[300px] object-cover" src={img}/>
+			<div className="absolute bottom-0 left-0 right-0 px-4 py-4 bg-black-500 opacity-90">
+				<div className="lg:flex lg:justify-between lg:items-center">
+					<h3 className="text-xl text-white-300 font-bold">{name}</h3>
+					<p className="text-sm text-white-300 line-through">R$ {oldPrice}</p>
+					<p className="text-xl text-white-300">R$ {price}</p>
 				</div>
 			</div>
 		</div>
