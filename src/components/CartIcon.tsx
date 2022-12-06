f2564ed635c41ecad6ea3a28496a205567a8586e
Koemy <francine.pns@gmail.com>
2022-12-05 21:49:36 -0300

    Use Link instead a (html generic link tag)

--- src/components/CartIcon.tsx
+++ src/components/CartIcon.tsx
@@ -1,14 +1,17 @@
 import {AiFillShopping} from 'react-icons/ai';
+import Link from 'next/link';
 
-function Cart() {
+function CartIcon() {
 	return (
 		<div className="fixed right-0 bottom-0 py-5 px-5">
 			<div
 				className="py-5 px-5 bg-grey-300 rounded-full cursor-pointer ease hover:ease-in-out duration-300 hover:bg-white-300 drop-shadow-lg">
-				<a href="../cart"><AiFillShopping className="text-black-500"/></a>
+				<Link href="/cart">
+					<AiFillShopping/>
+				</Link>
 			</div>
 		</div>
 	);
 }
 
-export default Cart;
+export default CartIcon;
