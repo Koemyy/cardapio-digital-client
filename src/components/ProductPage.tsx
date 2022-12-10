3bc3014415735f457f3592aa4e8f16befe9ff260
Koemy <francine.pns@gmail.com>
2022-12-09 22:51:34 -0300

    Change weird and horrible logic to a new one

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -1,5 +1,8 @@
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import {ShoppingBag} from 'phosphor-react';
+import Link from 'next/link';
+import {ArrowCircleLeft} from 'phosphor-react';
+import {useState} from 'react';
 
 interface ProductsContent {
     name: string,
@@ -13,34 +16,27 @@ interface ProductsContent {
 }
 
 function ProductPage({name, price, oldPrice, description, img, ingredient, tag, serve}: ProductsContent) {
-	if (typeof document !== 'undefined') {
-		// eslint-disable-next-line prefer-const
-		let buttonAdd = document.getElementById('add');
-		const buttonRemove = document.getElementById('remove');
-		// eslint-disable-next-line prefer-const
-		let label = document.getElementById('label'),
-			count = 0;
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		buttonAdd.onclick = function () {
-			count += 1;
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			label.innerHTML = String(+count);
-		};
-		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-		// @ts-ignore
-		buttonRemove.onclick = function () {
-			count -= 1;
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			label.innerHTML = String(+count);
-		};
-	}
+	const [counter, setCounter] = useState(0);
 
+	//increase counter
+	const increase = () => {
+		setCounter(count => count + 1);
+	};
+
+	const reset = () =>{
+		setCounter(0);
+	};
+	const decrease = () => {
+		if (counter > 0) {
+			setCounter(count => count - 1);
+		}
+	};
 
 	return (
 		<section className="lg:flex lg:items-center lg:justify-center bg-black-500">
+			<Link href="../">
+				<div className="top-0 lg:left-[23%] lg:text-orange-400 absolute py-5 px-4 text-black-500"><ArrowCircleLeft size={40}/></div>
+			</Link>
 			<div>
 				<img src={img} alt={name} className="h-[500px] w-full object-cover"/>
 				<div>
@@ -49,9 +45,9 @@ function ProductPage({name, price, oldPrice, description, img, ingredient, tag, 
 							className="relative bg-black-500 mt-[-150px] w-full rounded-tl-3xl rounded-tr-3xl pb-20 pt-1">
 							<div
 								className="right-0 absolute bg-white-300 px-3 mr-3 justify-center items-center flex py-2 mt-3 rounded-full">
-								<button id="remove">-</button>
-								<label id="label" className="px-2">1</label>
-								<button id="add">+</button>
+								<button onClick={decrease}>-</button>
+								<label className="px-2">{counter}</label>
+								<button onClick={increase}>+</button>
 							</div>
 							<div className="items-start pl-3 flex flex-col">
 								<h2 className="text-white-300 flex py-3 text-2xl">{name}</h2>
@@ -61,19 +57,21 @@ function ProductPage({name, price, oldPrice, description, img, ingredient, tag, 
 										className="text-sm line-through text-white-300 decoration-white-300">{oldPrice}</p>
 								</div>
 								<p className="mb-5 text-white-300">{description}</p>
-								<p className="mb-5 text-white-300 space">{ingredient}</p>
-								<div className="flex pt-3 pb-3">
-									<p>{tag}</p>
+								<p className="text-white-300 space">{ingredient}</p>
+								<div className="flex rounded-full bg-orange-500 px-5 my-3">
+									<p className="text-white-300">{tag}</p>
 								</div>
 								<div>
 									<p className="text-white-300">{serve}</p>
 								</div>
 							</div>
-							<button
-								className="flex right-0 absolute mr-3 text-center cursor-pointer bg-orange-500 py-3 px-5 rounded-3xl align-middle">
-								<label className="flex text-white-300"><ShoppingBag size="20"
-									weight="fill"/>Carrinho</label>
-							</button>
+							<Link href="../orders">
+								<button
+									className="flex right-0 absolute mr-3 text-center cursor-pointer bg-orange-500 py-3 px-5 rounded-3xl align-middle">
+									<label className="flex text-white-300"><ShoppingBag size="20"
+										weight="fill"/>Carrinho</label>
+								</button>
+							</Link>
 						</div>
 					</div>
 				</div>
