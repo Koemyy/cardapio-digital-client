fd293b6131da81370f38d782d1554e92dd1dea8b
Ueverson Ramos <ueverson.ramos@liax.tech>
2023-04-15 18:02:15 -0300

    remocao do cart no cookie caso não tenha token

--- src/pages/semToken.tsx
+++ src/pages/semToken.tsx
@@ -1,4 +1,7 @@
+import Methods from "../Service/CookieService"
+
 export default function SemToken(){
+    Methods.delete('cart')
     return(
         <div className="flex justify-center items-center h-screen">
         <p className="text-gray-500 text-xl">Sem token...</p>
