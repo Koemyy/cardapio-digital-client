fc24ba608683b42020f575160b1ae9c39aec8c05
Ueverson Ramos <ueverson.ramos@liax.tech>
2023-06-12 17:39:53 -0300

    adicionando a quantidade de cada item ao salvar compra no banco

--- src/Service/ProductService.tsx
+++ src/Service/ProductService.tsx
@@ -2,9 +2,10 @@ interface Produto {
     cli_id:  number
     pro_id: number
     ped_status : string
+    ped_quantidade : number
 }
 
-export async function salvarCompra({cli_id, pro_id, ped_status}: Produto) {
+export async function salvarCompra({cli_id, pro_id, ped_status, ped_quantidade}: Produto) {
 
     const url = 'http://localhost:3000/actions/salvarCompra';
    
@@ -14,7 +15,7 @@ export async function salvarCompra({cli_id, pro_id, ped_status}: Produto) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            cli_id, pro_id, ped_status
+            cli_id, pro_id, ped_status, ped_quantidade
         })
     }).catch((err) => err.message)
 
